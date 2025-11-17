186a6666d06fc8042b8bcf6fbac856eefb008a46
lenykhoirina <khoirina258719@student.smkn1kandeman.sch.id>
2025-11-17 11:42:38 +0700

    Add files via upload

---
+++ Praktik 4.7_Leny Khoirina_X PPLG 1/Praktik 4.7_Leny Khoirina_X PPLG 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik_4._7_Leny_Khoirina_X_PPLG_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan nilai: ");
+            int nilai = int.Parse(Console.ReadLine());
+
+            if (nilai >= 90)
+            {
+                Console.WriteLine("Predikat: A");
+            }
+            else if (nilai >= 75)
+            {
+                Console.WriteLine("Predikat: B");
+            }
+            else if (nilai >= 60)
+            {
+                Console.WriteLine("Predikat: C");
+            }
+            else
+            {
+                Console.WriteLine("Predikat: d");
+            }
+        }
+    }
+}
