dc604e260fef8e4452dc2c2df12182c44ac4f489
lenykhoirina <khoirina258719@student.smkn1kandeman.sch.id>
2025-11-17 11:37:19 +0700

    Add files via upload

---
+++ Praktik 4.5_Leny Khoirina_X PPLG 1/Praktik 4.5_Leny Khoirina_X PPLG 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik_4._5_Leny_Khoirina_X_PPLG_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan umur Anda: ");
+            int umur = int.Parse(Console.ReadLine());
+
+            if (umur >= 17)
+            {
+                Console.WriteLine("Anda sudah dewasa. ");
+            }
+            else
+            {
+                Console.WriteLine("Anda masih anak-anak. ");
+            }
+        }
+    }
+}
