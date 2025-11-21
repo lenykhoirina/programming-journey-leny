be8e2dfac739ddc4e0851f7fae4b256ea1caa6d2
lenykhoirina <khoirina258719@student.smkn1kandeman.sch.id>
2025-11-21 17:33:52 +0700

    Add files via upload

---
+++ praktik_5.8_Leny Khoirina_X PPLG 1/praktik_5.8_Leny Khoirina_X PPLG 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_5._8_Leny_Khoirina_X_PPLG_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ulangiPilihan;
+
+            do
+            {
+                // --- Blok Proses Yang Akan Diulang ---
+                Console.WriteLine("-----------------------------------");
+                Console.Write("Masukkan nama Anda: ");
+                string nama = Console.ReadLine();
+                Console.WriteLine($"Halo, {nama}! Proses telah selesai.");
+                // -----------------------------------------
+                // Pertanyaan untuk Mengulang 
+                Console.Write("Apakah Anda ingin mengulang lagi? (ya/tidak): ");
+                ulangiPilihan = Console.ReadLine().ToLower(); //Mengambil input dan mengubah ke huruf kecil
+
+                Console.WriteLine(); // Baris kosong untuk kerapian
+
+                // Kondisi diperiksa di akhir: Ulangi selama pengguna mengetik "ya"
+                } while (ulangiPilihan == "ya");
+                 Console.WriteLine("Terima kasih. Program selesai.");
+                }
+        }
+    }
+
